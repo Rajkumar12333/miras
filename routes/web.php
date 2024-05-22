95f05c94833c0fff568b394e0b2dfb8319b15922
Divya Priya <divya@fulgid.in>
2024-05-22 17:23:34 +0530

    web correction

--- routes/web.php
+++ routes/web.php
@@ -20,21 +20,12 @@ Route::get('/', function () {
 Route::get('/crud-view-page', [CrudController::class, 'crud_view_page'])->name("crud.view_page");
 Route::get('/crud-add-page', [CrudController::class, 'crud_add_page'])->name("crud.add_page");
 Route::get('/crud/{id}/edit-page', [CrudController::class, 'crud_edit_page'])->name('crud.edit_page');
-<<<<<<< HEAD
 Route::get('/add_applicant', function () {
     return view('add_applicant');
 })->name('add_applicant');
 Route::get('/visa_tracking',function () {
     return view('visa_tracking');
 })->name('visa_tracking');
-=======
-// Route::get('/add_applicant', function () {
-//     return view('add_applicant');
-// })->name('add_applicant');
-Route::get('/add_applicant_page', [DocumentController::class, 'add_page'])->name('applicant.add_page');
-Route::post('/add_applicant', [DocumentController::class, 'store'])->name('applicant.store');
-
->>>>>>> 9c3f0b9c2347713a888ab5dc48ec330a972b2dac
 
 Route::post('/crud-add', [CrudController::class, 'crud_add'])->name("crud.add");
 Route::post('/{id}/crud-update', [CrudController::class, 'crud_update'])->name("crud.update");
