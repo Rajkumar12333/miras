6a593aed04a0e68d309b1476147e0c9c3874028d
Rajkumar@fulgid <rajkumar2k21m@gmail.com>
2024-05-25 13:06:35 +0530

    dashboard error

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Applicant;
-
+use Illuminate\Support\Facades\Auth;
 class DashboardController extends Controller
 {
     //
